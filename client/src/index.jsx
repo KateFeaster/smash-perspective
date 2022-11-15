29856d3f82a985d6f3746a636bbe50b58efd4d18
KateFeaster <zencornhopper@gmail.com>
2022-11-14 18:39:01 -0800

    Created app component skeleton

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const Index = () => {
+
+function Index() {
   return <div>WELCOME TO REACT APP!</div>;
-};
+}
 ReactDOM.render(<Index />, document.getElementById('app'));
