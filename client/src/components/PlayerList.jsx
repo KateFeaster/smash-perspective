7babc93fdf5c1c1c56c1048b55f2cbeb669ffdcc
KateFeaster <zencornhopper@gmail.com>
2022-11-14 23:35:13 -0800

    Added dividers

--- client/src/components/PlayerList.jsx
+++ client/src/components/PlayerList.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
 import PlayerListEntry from './PlayerListEntry';
 
 function PlayerList({ players }) {
   return (
     <List>
-      {players.map((player) => (
-        <PlayerListEntry player={player} key={player.id} />
+      {players.map((player, index) => (
+        <React.Fragment key={player.id}>
+          <PlayerListEntry player={player} />
+          {index !== players.length - 1 && <Divider />}
+        </React.Fragment>
       ))}
     </List>
   );
