8cd7c2fd1861eab16a45d084b72efd5387d80af5
KateFeaster <zencornhopper@gmail.com>
2022-11-14 23:34:29 -0800

    Moved sample data to separate file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import PlayerList from './PlayerList';
-
-const sampleList = [
-  {
-    id: 1,
-    gamerTag: 'Chronos',
-    image:
-      'https://www.bing.com/th?pid=Sgg&qlt=100&u=https%3A%2F%2Fimages.start.gg%2Fimages%2Fuser%2F10213%2Fimage-c40810b15eb9654250288324f6f198f0-optimized.png&ehk=y%2BnUr%2BgGC2G2zW0NGOtkVDLyAmzVFUOD8nwRUNhNM4Y%3D&w=160&h=160&r=0&c=3',
-  },
-  { id: 2, gamerTag: 'Shoe', prefix: 'Pre' },
-];
+import sampleList from '../sampleData/samplePlayers';
 
 function App() {
-  return <PlayerList players={sampleList} />;
+  return (
+    <PlayerList
+      players={sampleList.data.event.entrants.nodes.map((node) => ({
+        id: node.participants[0].user.player.id,
+        gamerTag: node.participants[0].user.player.gamerTag,
+        prefix: node.participants[0].user.player.prefix,
+        image:
+          node.participants[0].user.images[0] &&
+          node.participants[0].user.images[0].url,
+      }))}
+    />
+  );
 }
 
 export default App;
---
+++ client/src/sampleData/samplePlayers.js
@@ -0,0 +1,462 @@
+module.exports = {
+  data: {
+    event: {
+      entrants: {
+        nodes: [
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Shuton',
+                    prefix: 'SST',
+                    id: 226829,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/134839/image-dd89d994a9e9811b70fb4d18214f7a56.png?ehk=6griyBz8Z%2BNHBkgcKvczeYpQUUlZW2HPygHmNCLrtmQ%3D&ehkOptimized=WYSGIPZxZO1BH3l1rkWXQukNLz4jf8LzMZ%2F9YrLtxmI%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Kameme',
+                    prefix: 'RayRoadGaming',
+                    id: 1408155,
+                  },
+                  images: [],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Gackt',
+                    prefix: 'ZETA',
+                    id: 484575,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/346254/image-8419c94c9645adb9a905fa8cbdb2bfa2.jpg?ehk=swt68KmmlPks2iaSY8bDEHR9kNPgDHdHSSlg9VlmNtw%3D&ehkOptimized=3XdFhZPE56C%2FMroHDwFQ%2BxBVBPRWEknPCYWP5gfqhwM%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Tweek',
+                    prefix: 'TSM FTX',
+                    id: 15768,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/10213/image-c40810b15eb9654250288324f6f198f0.png?ehk=eNXckPP25bccCa69XCL1332nzhmv20yls%2BZrLkuKUQU%3D&ehkOptimized=y%2BnUr%2BgGC2G2zW0NGOtkVDLyAmzVFUOD8nwRUNhNM4Y%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'KEN',
+                    prefix: 'SBI',
+                    id: 212701,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/285335/image-e1a02071945fd348b4394738062c870a.JPG?ehk=T0rBWYQc9hoht0TmQFLEliIS%2Be%2BOkECZaa0wb2iNfF8%3D&ehkOptimized=geSClQnm454a4zK%2F7h9sNCirGu%2Beb1UaNBAJpB6Wjss%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Yayzors',
+                    prefix: '',
+                    id: 285091,
+                  },
+                  images: [],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'FireThePyro',
+                    prefix: '',
+                    id: 390882,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/271395/image-7a3e77106fc4e4c5260796c84064a151.jpg?ehk=e0XEBy5%2FAlCM4c6oU5P%2Bi7nBZjoSD4TlDD8zk5c9qv0%3D&ehkOptimized=YeRn%2BnKy%2FuXLVcgap3gmG8TSZ0ZMAC16cIfCzGJNQew%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Silvi',
+                    prefix: 'DE',
+                    id: 489226,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/349696/image-31afd3362361f02c2f190c3a69957c3c.png?ehk=xB5jhq192qbvv%2FThLW7nL4JJ94y%2BsWfJ4PU1l7xPcAk%3D&ehkOptimized=XSDir6gX94MHp3MpMjjA7cFR0LPDcDOMpLX626q6C98%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Drean',
+                    prefix: '',
+                    id: 1062050,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/670975/image-6d7906c45f05d708489d1928d94a966a.png?ehk=%2Ft8XsQs1gzZCCAZAyi0gvHj%2By9BC18ayvL8iSfi0IZM%3D&ehkOptimized=uiLWwpsk2A%2FNGrv2WtMG9DfB4RoiJoOis3fT664ObJM%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Avenue',
+                    prefix: 'Panda',
+                    id: 566866,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/396729/image-ed9bd2b79a0ac7c539aee7e1886c031e.png?ehk=S6GThYj0WIUf2TPCN%2FxTwSDfV4V9751IhapKn0olSwA%3D&ehkOptimized=h2h%2Bh3IRMZFc%2FZ1R3vggN1KP7BfCwE21RPwQDAZHNyc%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Lyra',
+                    prefix: '',
+                    id: 49585,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/37607/image-6ff0a9fd11de45c744a19ef2ca8f8ba9.png?ehk=4WHrMEKzxGgacMGciUfBO79fIVEg7fJmcpYcq8hjLL4%3D&ehkOptimized=OOrJMHtgQLBTQInjK9tt8SWE5HI05BdS5yEvXvOzR2k%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Ice Goat',
+                    prefix: 'WGAFGC',
+                    id: 63818,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/46591/image-0147aeb55c6d55feb0da64f8d13634c1.png?ehk=DlJREcMTcEUZEnUVBrX%2BqJhB1Bf%2FS6Ws3sA669lt2cM%3D&ehkOptimized=2XpAu5K9Fv5gzdKyx5zYQhNo2ilZr5JUZoXTiL5xMMo%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Bleggaman',
+                    prefix: 'Bleg',
+                    id: 222959,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/137019/image-1fa0f836fbb127c5133766e4aee93f70.png?ehk=QHADDrG5i07Iq1IEZMDGyUKu0E%2FLZZz3IMKf2w69tpM%3D&ehkOptimized=qnlKI8TmAsm6jayFKnB%2FJXaAFYnAhxNPWWOqv7SzasI%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'pekwek',
+                    prefix: '',
+                    id: 1738602,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/1064245/image-868b458309f86f68198aa56afbe70d0b.jpg?ehk=pDG9pUhgJaaG%2FWjhnfOwQefTYoTcUgl81UtMGq36OEU%3D&ehkOptimized=lSkrjpWt6WwTMEm4q1GeDeDWhn%2FRGnfL5FyecP3Otf8%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Hidro',
+                    prefix: '',
+                    id: 1064010,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/673064/image-6d7eddec3879212949d77f5ee973e6d5.png?ehk=wpAGRGoVsGLZF1MsADQU%2FdJMYgqOadosVhW9W8YIFMY%3D&ehkOptimized=R3WuT5CivolYv3IwJc0dnHHpyZtFTjI28AqV7h%2BuAb0%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Mystery Sol',
+                    prefix: '',
+                    id: 705840,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/400272/image-0bbbd9c6ad8295ebe852da8238260a27.jpg?ehk=RAQ0wIT2qCYVouKpMb5E8osVrWAZf03Uhrr5xhXRXjY%3D&ehkOptimized=G942kgNxiH%2BJtQg7rGNR2vYNCMFn%2FeBc1QlrhYhaXKs%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Sensory',
+                    prefix: '',
+                    id: 1573588,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/967440/image-6d2f35d4cba65f2191d8d6ad43872bbb.jpg?ehk=JZ229L0FwYoY240unG6U1G6txbnaVm7AEPPWKVYjP1U%3D&ehkOptimized=TtlbPl9Z6pBC2cIp5LLQ%2BB5pmRMBEEbe1VQr8gojsnI%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Game 3',
+                    prefix: null,
+                    id: 2031015,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/1298158/image-bdc092793078cd738893fd17d1e04e83.png?ehk=KunDRcSZCIV1mhxEFfK43c%2FwbYwZdvTh51ldQAfzsV4%3D&ehkOptimized=zkbCBUot96%2FAnE9P2sFSrzu6EHYXLAXtnpEwDl1f7Zs%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Neos',
+                    prefix: 'CWA',
+                    id: 156283,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/93213/image-5e29c42bcaab1024caab3e20ef38e1cc.jpg?ehk=KvxPMnwHjsy8WE8gMzzLj1l9oCwRhY4c0NcE10C%2Bp8I%3D&ehkOptimized=jH%2BOTyQImtYznAyMJdFGjMLjgwDtW3TIwN27vg8337o%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Dii',
+                    prefix: 'RLz | MSFT',
+                    id: 1399572,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/876022/image-312ecfb0d2d930950d812450bc0cd23d.jpg?ehk=fYtPNl45sZMk%2BIv1oao3sUivkLfnoCibbeQUEu%2FwtIU%3D&ehkOptimized=eYOcJq0bEWDwkaSEX7xprZcMEIiOVT%2F9DeS094Ij34E%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'thinkin mikyu',
+                    prefix: '',
+                    id: 486903,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/347919/image-aa5041d3217e01c3b62dd873ef0e11b8.jpg?ehk=LUvYSbk4%2FQs1dQfMkZuY2Ph2RJcmypyVJhnSOrnbazY%3D&ehkOptimized=nsnOwu7pUj8ywXtZNA68tNWpa%2BzpGTPieLIMsk2Y%2FhU%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'kudaltrix',
+                    prefix: null,
+                    id: 1014668,
+                  },
+                  images: [],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'Arpeggio',
+                    prefix: 'BHAM!',
+                    id: 540874,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/380950/image-fe42059680f0af1cda7dd7ec672766a9.jpg?ehk=i4NTLSfrOceRkRvABDCyyiRAawn2n3%2FJOBRf%2FWFXQf4%3D&ehkOptimized=pId2%2BgWIzAF1eC6vCFIYU3qz%2FTjz5ThjHgjfHHPCcm8%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'methaneBrain',
+                    prefix: null,
+                    id: 2069286,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/1329037/image-46565bf3a2a67f35013171570c5f4a31.jpg?ehk=2WTOw%2FpS3Bwf8gRDMiCX9rktyf%2FfZhg17TfXhdar9RM%3D&ehkOptimized=CtyZwQfvuN3dg4aOVdeGyct7DhrgwgyqGkZ2XVnHYzQ%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+          {
+            participants: [
+              {
+                user: {
+                  player: {
+                    gamerTag: 'McCoy',
+                    prefix: null,
+                    id: 1740798,
+                  },
+                  images: [
+                    {
+                      url: 'https://images.start.gg/images/user/1066104/image-d82cf463d829418c11076f6126210591.jpg?ehk=GbVmCtBgMvG4y%2B3LtQbK2fvDoiNXQ1qprt1ygY22bZc%3D&ehkOptimized=H8MS32biB47Uw9wMrnJwiNfFq13Vnszuxl9RhWzK0dI%3D',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        ],
+      },
+    },
+  },
+  extensions: {
+    cacheControl: {
+      version: 1,
+      hints: [
+        {
+          path: ['event'],
+          maxAge: 60,
+          scope: 'PRIVATE',
+        },
+      ],
+    },
+    queryComplexity: 76,
+  },
+  actionRecords: [],
+};
