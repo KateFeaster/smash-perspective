42b69635efca91682db12d4355d365b4ae3651ac
KateFeaster <zencornhopper@gmail.com>
2022-11-15 15:55:54 -0800

    Moved players and matches into arrays of their class

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
+import Grid from '@mui/material/Unstable_Grid2';
 import PlayerList from './PlayerList';
 import sampleList from '../sampleData/samplePlayers';
+import sampleMatches from '../sampleData/sampleMatches';
+import PlayerDetails from './PlayerDetails';
+import Player from '../Player';
+import Match from '../Match';
+
+const players = sampleList.data.event.entrants.nodes.map(
+  (node) =>
+    new Player({
+      id: node.id,
+      gamerTag: node.participants[0].user.player.gamerTag,
+      prefix: node.participants[0].user.player.prefix,
+      profileImage:
+        node.participants[0].user.images[0] &&
+        node.participants[0].user.images[0].url,
+      seed: node.initialSeedNum,
+      placement: node.standing.placement,
+    })
+);
+
+const matches = sampleMatches.data.entrant.paginatedSets.nodes.map(
+  (node) =>
+    new Match({
+      fullRoundText: node.fullRoundText,
+      phase: node.phaseGroup.phase.name,
+      pool: node.phaseGroup.displayIdentifier,
+      opponentId: node.slots.find(
+        (slot) => slot.entrant.id !== sampleMatches.data.entrant.id
+      ),
+      displayScore: node.displayScore,
+      id: node.id,
+    })
+);
+
+players[0].matches = matches;
 
 function App() {
   return (
-    <PlayerList
-      players={sampleList.data.event.entrants.nodes.map((node) => ({
-        id: node.participants[0].user.player.id,
-        gamerTag: node.participants[0].user.player.gamerTag,
-        prefix: node.participants[0].user.player.prefix,
-        image:
-          node.participants[0].user.images[0] &&
-          node.participants[0].user.images[0].url,
-      }))}
-    />
+    <Grid container>
+      <Grid xs={3}>
+        <PlayerList players={players} />
+      </Grid>
+      <Grid xs={8}>
+        <PlayerDetails player={players[0]} entrantCount={600} />
+      </Grid>
+    </Grid>
   );
 }
 
