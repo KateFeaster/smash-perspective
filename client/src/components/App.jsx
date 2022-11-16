48470ad85ebeffcf579347427e76cbc77608476a
KateFeaster <zencornhopper@gmail.com>
2022-11-16 14:29:15 -0800

    Changed app to get players from API call

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,46 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 import Grid from '@mui/material/Unstable_Grid2';
 import PlayerList from './PlayerList';
-import sampleList from '../sampleData/samplePlayers';
-import sampleMatches from '../sampleData/sampleMatches';
+// import sampleMatches from '../sampleData/sampleMatches';
 import PlayerDetails from './PlayerDetails';
 import Player from '../Player';
 import Match from '../Match';
 
-const players = sampleList.data.event.entrants.nodes.map(
-  (node) =>
-    new Player({
-      id: node.id,
-      gamerTag: node.participants[0].user.player.gamerTag,
-      prefix: node.participants[0].user.player.prefix,
-      profileImage:
-        node.participants[0].user.images[0] &&
-        node.participants[0].user.images[0].url,
-      seed: node.initialSeedNum,
-      placement: node.standing.placement,
-    })
-);
+// const matches = sampleMatches.data.entrant.paginatedSets.nodes.map(
+//   (node) =>
+//     new Match({
+//       fullRoundText: node.fullRoundText,
+//       phase: node.phaseGroup.phase.name,
+//       pool: node.phaseGroup.displayIdentifier,
+//       opponentId: node.slots.find(
+//         (slot) => slot.entrant.id !== sampleMatches.data.entrant.id
+//       ).entrant.id,
+//       displayScore: node.displayScore,
+//       id: node.id,
+//       won: node.winnerId
+//         ? node.winnerId === sampleMatches.data.entrant.id
+//         : null,
+//     })
+// );
 
-const matches = sampleMatches.data.entrant.paginatedSets.nodes.map(
-  (node) =>
-    new Match({
-      fullRoundText: node.fullRoundText,
-      phase: node.phaseGroup.phase.name,
-      pool: node.phaseGroup.displayIdentifier,
-      opponentId: node.slots.find(
-        (slot) => slot.entrant.id !== sampleMatches.data.entrant.id
-      ).entrant.id,
-      displayScore: node.displayScore,
-      id: node.id,
-      won: node.winnerId
-        ? node.winnerId === sampleMatches.data.entrant.id
-        : null,
-    })
-);
-
-players[0].matches = matches;
+// players[0].matches = matches;
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(-1);
+  const [currentPlayer, setCurrentPlayer] = useState(
+    new Player({ placement: 0, seed: 0 })
+  );
+
+  useEffect(
+    () => {
+      if (totalPages === -1 || page <= totalPages) {
+        Axios.get(`/players?page=${page}`).then((response) => {
+          const newPlayers = response.data.data.event.entrants.nodes.map(
+            (node) =>
+              new Player({
+                id: node.id,
+                gamerTag: node.participants[0].user.player.gamerTag,
+                prefix: node.participants[0].user.player.prefix,
+                profileImage:
+                  node.participants[0].user.images[0] &&
+                  node.participants[0].user.images[0].url,
+                seed: node.initialSeedNum,
+                placement: node.standing.placement,
+              })
+          );
+
+          setPage(page + 1);
+          setTotalPages(response.data.data.event.entrants.pageInfo.totalPages);
+          setPlayers([...players, ...newPlayers]);
+          if (currentPlayer.id === undefined) {
+            setCurrentPlayer(newPlayers[0]);
+          }
+        });
+      }
+    },
+    [
+      /* page */
+    ]
+  );
+
   return (
     <Grid container spacing={2}>
       <Grid xs={3}>
@@ -48,7 +73,7 @@ function App() {
       </Grid>
       <Grid xs={8}>
         <PlayerDetails
-          player={players[0]}
+          player={currentPlayer}
           entrantCount={600}
           players={players}
         />
